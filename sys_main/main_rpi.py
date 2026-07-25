2b5011af99a484ad6fb4b5b65eaa2ff04c9b260e
Ceddy <lonapse456@gmail.com>
2026-07-25 21:39:23 +0800

    enabled full screen, hahaha nalimutan ibalik

--- sys_main/main_rpi.py
+++ sys_main/main_rpi.py
@@ -7,8 +7,8 @@ import subprocess
 
 from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemandmulti')
-#Config.set('graphics', 'fullscreen', 'auto')
-#Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'resizable', False)
 
 from kivymd.app import MDApp
 from kivy.lang import Builder
