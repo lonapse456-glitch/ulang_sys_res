a04f64444da9063f3ac1b7d6f9c40a2b78f5ccf8
Ceddy <lonapse456@gmail.com>
2026-08-09 18:04:41 +0800

    a fix to disabling picam2 preview

--- sys_main/main_rpi.py
+++ sys_main/main_rpi.py
@@ -44,7 +44,7 @@ import random #temporary
 # HARDWARE GPIO SETUP (Fail-safe for Windows/Mac testing)
 # ---------------------------------------------------------
 
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 
 '''try:
     from gpiozero import Button as HardwareButton
@@ -1409,8 +1409,8 @@ class UlangSystemApp(MDApp):
             main={"size": (1920,1080), "format": "RGB888"}
         )
         self.picam2.configure(vd_config)
-        #self.picam2.start()
-        self.picam2.start(preview=None)
+        self.picam2.start_preview(Preview.NULL)
+        self.picam2.start()
         #self.capture = cv2.VideoCapture(0) main_win.py
         self.model = YOLO("models/pre-trained/ulangn-obb_v3-1_ncnn_model")
         self._camera_ready()
